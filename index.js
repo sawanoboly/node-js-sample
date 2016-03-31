dc21bdc71f1b1459f6d9eaf5192acad36bdae6d1
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-31 12:51:14 +0900

    World to Node.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! ' + version)
+  response.send('Hello Node.js! ' + version)
 })
 
 app.listen(app.get('port'), function() {
